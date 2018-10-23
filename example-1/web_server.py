2784802ee39c70d6b00bb9eb674b1b0d10bc1faf
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2018-10-22 21:02:52 -0400

    Update web server to process_request

--- example-1/web_server.py
+++ example-1/web_server.py
@@ -2,14 +2,22 @@ import socket
 
 HOST = ''    # Listen on any interface
 PORT = 8000  # Listen on port 8000
+MAX_SIZE = 1024
+
+
+def process_request(request):
+    print("Received", request)
+    return 'Hello World'
+
 
 with socket.socket() as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        data = conn.recv(1024)
-        print("Received", data)
-        conn.sendall(b'Hello World')
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
+            data = conn.recv(MAX_SIZE)
+            response = process_request(data)
+            conn.sendall(response.encode('utf-8'))
